5c78915b2dbb38fcda4bdd968e8361b72eb1e98a
ferdouszislam <ferdouszislam@gmail.com>
2020-12-20 12:50:37 +0600

    Midterm, Theory #30: App XML creation XMLs created for all three activities- MainActivity, EncryptMessageActivity, DecryptMessageActivity.

--- Assignments/Theory/Midterm/SecretTextApp/app/src/main/java/com/nsu/ece/ferdouszislam/cse486l/sec01/secrettextapp/DecryptMessageActivity.java
+++ Assignments/Theory/Midterm/SecretTextApp/app/src/main/java/com/nsu/ece/ferdouszislam/cse486l/sec01/secrettextapp/DecryptMessageActivity.java
@@ -2,25 +2,17 @@ package com.nsu.ece.ferdouszislam.cse486l.sec01.secrettextapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DecryptMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void encryptMessageClick(View view) {
-
-        startActivity(new Intent(this, EncryptMessageActivity.class));
+        setContentView(R.layout.activity_decrypt_message);
     }
 
     public void decryptMessageClick(View view) {
-
-        startActivity(new Intent(this, DecryptMessageActivity.class));
     }
 }
---
+++ Assignments/Theory/Midterm/SecretTextApp/app/src/main/java/com/nsu/ece/ferdouszislam/cse486l/sec01/secrettextapp/EncryptMessageActivity.java
@@ -0,0 +1,14 @@
+package com.nsu.ece.ferdouszislam.cse486l.sec01.secrettextapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncryptMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypt_message);
+    }
+}
