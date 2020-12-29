b14a638ed7ea5708e94d08eb94000d00466d6f16
ferdouszislam <ferdouszislam@gmail.com>
2020-12-29 23:18:16 +0600

    Project Enhancement #11: Up button added to CommonLoginActivity
    Toolbar with up button and no title added to CommonLoginActivity.

--- Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/CommonLoginActivity.java
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/CommonLoginActivity.java
@@ -24,7 +24,24 @@ public class CommonLoginActivity extends AppCompatActivity {
 
     private void init() {
 
+        setupToolbar();
+
         mUserType = getIntent().getStringExtra(UserTypeChoiceActivity.SELECTED_USER_TYPE_KEY);
         Log.d(TAG, "init: user type = "+mUserType);
+    }
+
+    /*
+    setup the toolbar with back button
+     */
+    private void setupToolbar() {
+
+        Toolbar myChildToolbar = findViewById(R.id.commonLogin_Toolbar);
+        setSupportActionBar(myChildToolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+        // don't show default(app_name) title
+        ab.setDisplayShowTitleEnabled(false);
     }
 }
