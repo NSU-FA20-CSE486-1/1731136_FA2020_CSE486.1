e48775d9d0476bd995e844e8d91a2ef6c34799f9
ferdouszislam <ferdouszislam@gmail.com>
2020-12-29 21:44:03 +0600

    Project Enhancement #11: User picker page XML designed

--- Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/MainActivity.java
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.ferdouszislam.nsu.cse486.sec01.homemealapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * Activity for welcome page
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    "GET STARTED" button click listener
+     */
+    public void getStartedClick(View view) {
+
+        startActivity(new Intent(this, UserTypeChoiceActivity.class));
+
+        // stop opening this activity on back press
+        finish();
     }
 }
