975f56237e1db80e022606782e76ac6b1ea78b6e
ferdouszislam <ferdouszislam@gmail.com>
2020-12-30 04:51:44 +0600

    Project Enhancement #11: Chef Signup screen XML created.

---
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/ChefSignupActivity.java
@@ -0,0 +1,40 @@
+package com.ferdouszislam.nsu.cse486.sec01.homemealapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class ChefSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_signup);
+
+        init();
+    }
+
+    private void init() {
+
+        setupToolbar();
+    }
+
+    /*
+    setup the toolbar with back button
+     */
+    private void setupToolbar() {
+
+        Toolbar myChildToolbar = findViewById(R.id.chefSignup_Toolbar);
+        setSupportActionBar(myChildToolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        if(ab!=null) {
+            // Enable the Up button
+            ab.setDisplayHomeAsUpEnabled(true);
+            // don't show default(app_name) title
+            ab.setDisplayShowTitleEnabled(false);
+        }
+    }
+}
--- Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/CommonLoginActivity.java
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/CommonLoginActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.utils.UserType;
 
 public class CommonLoginActivity extends AppCompatActivity {
 
@@ -40,15 +43,37 @@ public class CommonLoginActivity extends AppCompatActivity {
         setSupportActionBar(myChildToolbar);
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
-        // Enable the Up button
-        ab.setDisplayHomeAsUpEnabled(true);
-        // don't show default(app_name) title
-        ab.setDisplayShowTitleEnabled(false);
+        if(ab!=null) {
+            // Enable the Up button
+            ab.setDisplayHomeAsUpEnabled(true);
+            // don't show default(app_name) title
+            ab.setDisplayShowTitleEnabled(false);
+        }
     }
 
     /*
     "Login" button click listener
      */
     public void loginClick(View view) {
+    }
+
+    public void dontHaveAnAccountClicked(View view) {
+
+        if(mUserType.equals(UserType.CUSTOMER)){
+
+
+        }
+
+        else if(mUserType.equals(UserType.CHEF)){
+
+            startActivity(new Intent(this, ChefSignupActivity.class));
+        }
+
+        else if(mUserType.equals(UserType.DELIVERY_GUY)){
+
+
+        }
+
+        else Log.d(TAG, "dontHaveAnAccountClicked: error! no such user type");
     }
 }
