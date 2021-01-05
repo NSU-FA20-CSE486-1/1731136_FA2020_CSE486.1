6af590ae52176dbb0c54373a23d12eb1a540115c
ferdouszislam <ferdouszislam@gmail.com>
2021-01-06 04:00:17 +0600

    Theory, Quiz #34: androidTest file package name change test edited

--- Assignments/Theory/Quiz-2/Bangla Dictonary/app/src/androidTest/java/com/ferdouszislam/nsu/cse486/sec01/bangladictonary/ExampleInstrumentedTest.java
+++ Assignments/Theory/Quiz-2/Bangla Dictonary/app/src/androidTest/java/com/ferdouszislam/nsu/cse486/sec01/bangladictonary/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.ferdouszislam.nsu.cse486.sec01.test", appContext.getPackageName());
+        assertEquals("com.ferdouszislam.nsu.cse486.sec01.bangladictonary", appContext.getPackageName());
     }
 }
