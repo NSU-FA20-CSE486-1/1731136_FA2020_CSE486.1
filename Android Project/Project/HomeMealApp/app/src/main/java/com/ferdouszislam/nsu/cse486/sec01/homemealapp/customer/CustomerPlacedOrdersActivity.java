91aac46a002d3f0eedb6d56fd4e385b127c3ade5
ferdouszislam <ferdouszislam@gmail.com>
2021-01-06 21:14:03 +0600

    Project Enhancement #12: Customer App placed orders page XML added

--- Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/customer/CustomerPlacedOrdersActivity.java
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/customer/CustomerPlacedOrdersActivity.java
@@ -4,18 +4,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.ferdouszislam.nsu.cse486.sec01.homemealapp.R;
 
-public class CustomerMenuActivity extends AppCompatActivity {
+public class CustomerPlacedOrdersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_menu);
+        setContentView(R.layout.activity_customer_placed_orders);
 
         init();
     }
@@ -30,7 +28,7 @@ public class CustomerMenuActivity extends AppCompatActivity {
      */
     private void setupToolbar() {
 
-        Toolbar myChildToolbar = findViewById(R.id.customerMenu_Toolbar);
+        Toolbar myChildToolbar = findViewById(R.id.customerPlacedOrders_Toolbar);
         setSupportActionBar(myChildToolbar);
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
@@ -38,17 +36,7 @@ public class CustomerMenuActivity extends AppCompatActivity {
             // Enable the Up button
             ab.setDisplayHomeAsUpEnabled(true);
             // set toolbar title
-            ab.setTitle(R.string.menu_title);
+            ab.setTitle(R.string.placed_orders_title);
         }
-    }
-
-
-    /*
-    Menu option click listeners
-     */
-
-    public void placedOrdersClick(View view) {
-
-        startActivity(new Intent(this, CustomerPlacedOrdersActivity.class));
     }
 }
