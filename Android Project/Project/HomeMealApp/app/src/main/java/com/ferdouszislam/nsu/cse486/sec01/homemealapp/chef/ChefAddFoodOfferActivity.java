66092bb2268db92337dc1e1743d314c590a2b2d7
ferdouszislam <ferdouszislam@gmail.com>
2021-01-10 03:14:43 +0600

    Project, Enhancement #15: Image taken and stored locally(temporary file)

--- Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/chef/ChefAddFoodOfferActivity.java
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/chef/ChefAddFoodOfferActivity.java
@@ -1,15 +1,42 @@
 package com.ferdouszislam.nsu.cse486.sec01.homemealapp.chef;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.ferdouszislam.nsu.cse486.sec01.homemealapp.R;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.chef.imageUpload.CapturedImage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ChefAddFoodOfferActivity extends AppCompatActivity {
+
+    private static final String TAG = "CAFOA-debug";
+
+    // request code for camera intent
+    private static final int REQUEST_IMAGE_CAPTURE = 589;
+
+    // auth to access taken image file
+    private static final String FILE_PROVIDER_AUTHORITY = "com.ferdouszislam.nsu.cse486.sec01.homemealapp.fileprovider";
+
+    // ui
+    private ImageView mFoodImageView;
+
+    // model for captured image
+    private CapturedImage mImage;
+    private boolean mImageWasTaken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +49,9 @@ public class ChefAddFoodOfferActivity extends AppCompatActivity {
     private void init() {
 
         setupToolbar();
+
+        mFoodImageView = findViewById(R.id.chef_addFoodOffer_foodOffer_ImageView);
+
     }
 
     /*
@@ -42,6 +72,78 @@ public class ChefAddFoodOfferActivity extends AppCompatActivity {
     }
 
     public void addFoodPhotoClick(View view) {
+
+        try {
+
+            mImage = CapturedImage.build(this);
+
+            dispatchTakePictureIntent(mImage);
+
+        } catch (IOException e) {
+
+            commonErrorUI();
+
+            Log.d(TAG, "takePhotoClick: error->"+e.getMessage());
+        }
+    }
+
+    /**
+     * open default camera app to take an image
+     * @param image model for image to be capture, with info of temporarily created local file info
+     */
+    private void dispatchTakePictureIntent(CapturedImage image) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = image.getmPhotoFile();
+
+            if (photoFile != null) {
+
+                Uri photoURI = FileProvider.getUriForFile(
+                        this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile
+                );
+
+                image.setmPhotoUri(photoURI);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode!=RESULT_OK) return;
+
+        switch (requestCode){
+
+            case REQUEST_IMAGE_CAPTURE:
+
+                mImageWasTaken = true;
+
+                mFoodImageView.setImageURI(mImage.getmPhotoUri());
+
+                // TODO: remove toast
+                Toast.makeText(this, "image taken!", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+    }
+
+    /*
+    UI event for common errors
+     */
+    private void commonErrorUI() {
+
+        Toast.makeText(this, R.string.an_unexpected_error_occurred, Toast.LENGTH_SHORT)
+                .show();
     }
 
     public void offerFoodClick(View view) {
