423a58157da65d6383ceaa56ec82f728c0acee00
ferdouszislam <ferdouszislam@gmail.com>
2021-01-14 23:18:46 +0600

    Project, Enhancement #21: ChefPlacedOrdersActivity's recycler view connected with Adapter

--- Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/chef/ChefPlacedOrdersActivity.java
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/chef/ChefPlacedOrdersActivity.java
@@ -3,12 +3,58 @@ package com.ferdouszislam.nsu.cse486.sec01.homemealapp.chef;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ferdouszislam.nsu.cse486.sec01.homemealapp.R;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.auth.Authentication;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.auth.AuthenticationUser;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.auth.FirebaseEmailPasswordAuthentication;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.chef.recyclerViewAdapters.ChefPlacedOrdersAdapter;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.models.FoodOrder;
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.utils.SessionUtil;
 
-public class ChefPlacedOrdersActivity extends AppCompatActivity {
+public class ChefPlacedOrdersActivity extends AppCompatActivity implements ChefPlacedOrdersAdapter.CallerCallback {
+
+    private static final String TAG = "ChPOA-debug";
+
+    // ui
+    private TextView mNoOrdersPlacedTextView;
+    private RecyclerView mPlacedOrdersRecyclerView;
+    private ChefPlacedOrdersAdapter mPlacedOrdersAdapter;
+
+    // model data
+    private String mChefUid;
+
+    // variables for user authentication
+    private Authentication mAuth;
+    private Authentication.AuthenticationCallbacks mAuthCallbacks = new Authentication.AuthenticationCallbacks() {
+        @Override
+        public void onAuthenticationSuccess(AuthenticationUser user) {
+
+            mChefUid = user.getmUid();
+            loadRecyclerView(mChefUid);
+        }
+
+        @Override
+        public void onAuthenticationFailure(String message) {
+
+            Toast.makeText(ChefPlacedOrdersActivity.this, R.string.hard_logout, Toast.LENGTH_SHORT)
+                    .show();
+
+            SessionUtil.logoutNow(ChefPlacedOrdersActivity.this, mAuth);
+
+            Log.d(TAG, "onAuthenticationFailure: authentication failed -> "+message);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +67,19 @@ public class ChefPlacedOrdersActivity extends AppCompatActivity {
     private void init() {
 
         setupToolbar();
+
+        mNoOrdersPlacedTextView = findViewById(R.id.chef_noPlacedOrders_TextView);
+        mPlacedOrdersRecyclerView = findViewById(R.id.chefPlacedOrders_RecyclerView);
+
+        mAuth = new FirebaseEmailPasswordAuthentication(mAuthCallbacks);
+        mAuth.authenticateUser();
+    }
+
+    private void loadRecyclerView(String chefUid) {
+
+        mPlacedOrdersAdapter = new ChefPlacedOrdersAdapter(chefUid, this);
+        mPlacedOrdersRecyclerView.setAdapter(mPlacedOrdersAdapter);
+        mPlacedOrdersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     /*
@@ -38,5 +97,48 @@ public class ChefPlacedOrdersActivity extends AppCompatActivity {
             // set toolbar title
             ab.setTitle(R.string.placed_orders_title);
         }
+    }
+
+    @Override
+    public void onAcceptOrderClick(FoodOrder foodOrder) {
+
+    }
+
+    @Override
+    public void onRejectOrderClick(FoodOrder foodOrder) {
+
+    }
+
+    @Override
+    public void onPlacedOrdersListNotEmpty() {
+
+        mNoOrdersPlacedTextView.setVisibility(View.GONE);
+        mPlacedOrdersRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onFailedToLoadPlacedOrders() {
+
+        Toast.makeText(this, R.string.an_unexpected_error_occurred, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    public void contactCustomerClick(String phoneNumber) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else contactCustomerFailedUI();
+    }
+
+    private void contactCustomerFailedUI() {
+
+        Toast.makeText(this, R.string.call_failed, Toast.LENGTH_SHORT)
+                .show();
     }
 }
