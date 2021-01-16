7d8b13245c43bb79238b8f73f7bd8c91201f0989
ferdouszislam <ferdouszislam@gmail.com>
2021-01-17 03:56:20 +0600

    Project, Enhancement #38: BaseActivity created to detect internet connectivity
    Other activities that require to detect internet connectivity state need to extend BaseActivity and call enableInternetStatusFeedback(View view) to show a snackbar whenever internet is not available.

---
+++ Android Project/Project/HomeMealApp/app/src/main/java/com/ferdouszislam/nsu/cse486/sec01/homemealapp/BaseActivity.java
@@ -0,0 +1,69 @@
+package com.ferdouszislam.nsu.cse486.sec01.homemealapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ferdouszislam.nsu.cse486.sec01.homemealapp.broadcastReceivers.NetworkConnectionBroadcastReceiver;
+import com.google.android.material.snackbar.Snackbar;
+
+public class BaseActivity extends AppCompatActivity implements NetworkConnectionBroadcastReceiver.InternetStatusCallback {
+
+    private NetworkConnectionBroadcastReceiver mNetworkConnBroadcastReceiver = null;
+
+    private Snackbar mSnackbar;
+
+    private boolean mGiveInternetStatusFeedback = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void enableInternetStatusFeedback(View view){
+
+        mGiveInternetStatusFeedback = true;
+
+        mSnackbar = Snackbar.make(view, R.string.no_internet, Snackbar.LENGTH_INDEFINITE);
+        mSnackbar.setAction("dismiss", v -> mSnackbar.dismiss());
+
+        mNetworkConnBroadcastReceiver =
+                new NetworkConnectionBroadcastReceiver(this);
+        // register the broadcast receiver
+        registerReceiver(mNetworkConnBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(mNetworkConnBroadcastReceiver!=null && mGiveInternetStatusFeedback) {
+            // register the broadcast receiver
+            registerReceiver(mNetworkConnBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+
+        if(mNetworkConnBroadcastReceiver!=null && mGiveInternetStatusFeedback) unregisterReceiver(mNetworkConnBroadcastReceiver);
+
+        super.onStop();
+    }
+
+    @Override
+    public void onInternetAvailable() {
+
+        mSnackbar.dismiss();
+    }
+
+    @Override
+    public void onInternetNotAvailable() {
+
+        mSnackbar.show();
+    }
+}
